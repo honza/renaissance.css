d6c9d4d59561aac2d0a074c849eb6dc365a56981
Honza Pokorny <me@honza.ca>
2017-09-10 21:53:29 -0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# renaissance.css
+
+The perfectly-proportined renaissance page in CSS.
+
+## License
+
+MIT, short and sweet
---
+++ build.sh
@@ -0,0 +1,5 @@
+scss --sourcemap=none src/renaissance.scss renaissance.css
+scss --sourcemap=none --style compressed src/renaissance.scss renaissance.min.css
+
+sed -i '1i /* renaissance.css 0.1.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.css
+sed -i '1i /* renaissance.css 0.1.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.min.css
