fb2eb3cb7d8c55f2ea912d6cdbb3b1559c9ae2ad
Honza Pokorny <me@honza.ca>
2017-09-17 20:30:15 -0300

    Bump to 0.2.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 scss --sourcemap=none src/renaissance.scss renaissance.css
 scss --sourcemap=none --style compressed src/renaissance.scss renaissance.min.css
 
-sed -i '1i /* renaissance.css 0.1.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.css
-sed -i '1i /* renaissance.css 0.1.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.min.css
+sed -i '1i /* renaissance.css 0.2.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.css
+sed -i '1i /* renaissance.css 0.2.0 --- Copyright (c) 2017 Honza Pokorny <me@honza.ca> */' renaissance.min.css
 
 cp renaissance.css docs/
